d121a709bb4e288416a27ded4cc1f9b208f8bcf1
ravisumit33 <ravi.sumit33@gmail.com>
2024-08-25 01:21:44 +0530

    Add MOD & MOD_IS_PRIME constants inside mint class

--- modular-arithmetic.cpp
+++ modular-arithmetic.cpp
@@ -13,9 +13,6 @@ using namespace std;
 
 namespace modular {
 
-const int MOD = 998244353;
-const bool MOD_IS_PRIME = true;
-
 class mint {
 public:
   mint() : m_num(0) {}
@@ -127,65 +124,8 @@ public:
 
 private:
   int64_t m_num;
+  static constexpr int MOD = 998244353;
+  static constexpr bool MOD_IS_PRIME = true;
 };
-
-inline int64_t mod_add(int64_t x, int64_t y) {
-  return ((x % MOD) + (y % MOD)) % MOD;
-}
-
-inline int64_t mod_sub(int64_t x, int64_t y) {
-  return ((x % MOD) - (y % MOD) + MOD) % MOD;
-}
-
-inline int64_t mod_mul(int64_t x, int64_t y) {
-  return ((x % MOD) * (y % MOD)) % MOD;
-}
-
-int64_t mod_pow(int64_t x, int64_t y) {
-  int64_t result = 1;
-  x %= MOD;
-  if (x == 0)
-    return 0;
-  while (y > 0) {
-    if (y & 1)
-      result = (result * x) % MOD;
-    y >>= 1;
-    x = (x * x) % MOD;
-  }
-  return result;
-}
-
-inline int64_t mod_inv_prime(int64_t x) {
-  // This works only if MOD is a prime number.
-  return mod_pow(x, MOD - 2);
-}
-
-int64_t mod_inv(int64_t x) {
-  function<int64_t(int64_t, int64_t, int64_t &, int64_t &)> gcd_extended =
-      [&](int64_t a, int64_t b, int64_t &y, int64_t &z) {
-        if (a == 0) {
-          y = 0;
-          z = 1;
-          return b;
-        }
-        int64_t y1, z1;
-        int64_t g = gcd_extended(b % a, a, y1, z1);
-        y = y1 - (b / a) * z1;
-        z = y1;
-        return g;
-      };
-
-  int64_t y, z;
-  int64_t g = gcd_extended(x, MOD, y, z);
-  assert(g != 1); // x and MOD should be coprime for inverse to exist
-  return (y % MOD + MOD) % MOD;
-}
-
-int64_t mod_div(int64_t x, int64_t y) {
-  x %= MOD;
-  int64_t inv = mod_inv_prime(y);
-  // int64_t inv = mod_inv(y);
-  return (inv * x) % MOD;
-}
 
 } // namespace modular
